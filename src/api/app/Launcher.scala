b7b132652e8308e7dde9b111d11cca9f01451296
paq1 <p.paquin1016@laposte.net>
2023-10-23 11:03:05 +0200

    chore: fix probleme de laucher

--- src/api/app/Launcher.scala
+++ src/api/app/Launcher.scala
@@ -1,13 +1,5 @@
-import play.api.{ApplicationLoader, Environment, Play}
-
-// MKDMKD fixme trouver le moyen de lancer l'application via cette input pour faire du debug
 object Launcher {
   def main(args: Array[String]): Unit = {
-    val env = Environment.simple()
-    val context = ApplicationLoader.Context.create(env)
-    val loader = new MyLoader
-    val app = loader.load(context)
-
-    Play.start(app)
+    play.core.server.ProdServerStart.main(args)
   }
 }
