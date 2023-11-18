683aa7d63726a5025949c64b1ca6f93e86918334
Pierre Paquin <p.paquin1016@laposte.net>
2023-11-18 11:20:42 +0100

    refactor(controller): clean code

--- src/api/app/adressesExternes/fr/controllers/AdresseExterneFrWriteController.scala
+++ src/api/app/adressesExternes/fr/controllers/AdresseExterneFrWriteController.scala
@@ -1,15 +1,9 @@
 package adressesExternes.fr.controllers
 
 import adressesExternes.fr.commandHandler.CreateAdresseExterneFrHandler
-import adressesExternes.fr.events.{
-  ReferenceExterneFrCreated,
-  ReferencesExternesFrEvent
-}
+import adressesExternes.fr.events.{ReferenceExterneFrCreated, ReferencesExternesFrEvent}
 import adressesExternes.fr.services.AdressesExterneFrRepositoryMongo
-import adressesExternes.fr.states.{
-  CreateReferenceExterneFrState,
-  ReferencesExternesFrState
-}
+import adressesExternes.fr.states.{CreateReferenceExterneFrState, ReferencesExternesFrState}
 import cats.data.Validated
 import cats.data.Validated.{Invalid, Valid}
 import com.google.inject.Inject
@@ -36,7 +30,7 @@ class AdresseExterneFrWriteController @Inject() (
     )
   }
 
-  implicit val oreadInfo: Reads[InfoReferenceExterneFr] =
+  implicit val oreadInfoExterneFr: Reads[InfoReferenceExterneFr] =
     (json: JsValue) => {
       JsSuccess(
         InfoReferenceExterneFr(
@@ -54,7 +48,7 @@ class AdresseExterneFrWriteController @Inject() (
       )
     }
 
-  implicit val oread: Reads[CreateAdresseExterneFrCommand] =
+  implicit val oreadCreateCommand: Reads[CreateAdresseExterneFrCommand] =
     (json: JsValue) => {
       JsSuccess(
         CreateAdresseExterneFrCommand(
@@ -63,7 +57,7 @@ class AdresseExterneFrWriteController @Inject() (
       )
     }
 
-  implicit val owrite: Writes[ReferencesExternesFrEvent] = {
+  implicit val owriteCreateEvent: Writes[ReferencesExternesFrEvent] = {
     case ReferenceExterneFrCreated(infoReferenceExterneFr, at, by) =>
       Json.obj(
         "numeroRue" -> infoReferenceExterneFr.numeroRue,
@@ -74,7 +68,7 @@ class AdresseExterneFrWriteController @Inject() (
     case _ => Json.obj()
   }
 
-  implicit val owritestate: Writes[ReferencesExternesFrState] = {
+  implicit val owriteCreateState: Writes[ReferencesExternesFrState] = {
     case CreateReferenceExterneFrState(infoReferenceExterneFr, kind) =>
       Json.obj(
         "numeroRue" -> infoReferenceExterneFr.numeroRue,
@@ -84,46 +78,37 @@ class AdresseExterneFrWriteController @Inject() (
     case _ => Json.obj()
   }
 
-  val createHandler = new CreateAdresseExterneFrHandler()
+  private val createHandler = new CreateAdresseExterneFrHandler()
 
   def create(): Action[AnyContent] = Action.async {
     implicit request: Request[AnyContent] =>
       val event: Future[ValidatedErr[ReferencesExternesFrEvent]] =
-        request.body.asJson
-          .map { cmdJson =>
-            val cmd = cmdJson.as[CreateAdresseExterneFrCommand]
-            createHandler.onCommand(cmd)
-          }
-          .getOrElse(Future.successful(Invalid("bruh")))
+        createACreateEvent(request.body.asJson)
 
-      val state: Future[ValidatedErr[CreateReferenceExterneFrState]] = event
+      val state: Future[ValidatedErr[ReferencesExternesFrState]] =
+        fromEventToState(event)
+
+      val savingEventsAndState =
+        saveEventAndStateInStoreAndJournal(event, state)
+
+      savingEventsAndState
         .map {
-          case Validated.Valid(a) =>
-            a match {
-              case ref: ReferenceExterneFrCreated => Valid(reducer(ref))
-              case _                              => Invalid("Illegal transition state")
-            }
-          case i @ Invalid(e) => i
+          case Valid((event, state)) =>
+            Ok(
+              Json.obj(
+                "data" -> Json.obj("attributes" -> Json.toJson(event))
+              )
+            )
+          case Invalid(e) =>
+            InternalServerError(
+              Json.obj(
+                "error" -> Json.obj(
+                  "status" -> "500",
+                  "message" -> e.toString
+                )
+              )
+            )
         }
-
-      for {
-        e: ReferencesExternesFrEvent <- event
-          .flatMap {
-            case Valid(a)   => Future.successful(a)
-            case Invalid(e) => Future.failed(new Exception(s"$e"))
-          }
-        s <- state
-          .flatMap {
-            case Valid(a)   => Future.successful(a)
-            case Invalid(e) => Future.failed(new Exception(s"$e"))
-          }
-        savingStore <- store.insertOne(s)
-        savingJournal <- Future.successful(()) // MKDMKD todo save in journal
-      } yield Ok(
-        Json.obj(
-          "data" -> Json.obj("attributes" -> Json.toJson(e))
-        )
-      )
   }
 
   def getAll(): Action[AnyContent] = Action.async {
@@ -136,6 +121,53 @@ class AdresseExterneFrWriteController @Inject() (
             JsArray(jsList)
           )
         }
+  }
+
+  private def saveEventAndStateInStoreAndJournal(
+      event: Future[ValidatedErr[ReferencesExternesFrEvent]],
+      state: Future[ValidatedErr[ReferencesExternesFrState]]
+  ): Future[
+    ValidatedErr[(ReferencesExternesFrEvent, ReferencesExternesFrState)]
+  ] = {
+    for {
+      e <- event
+        .flatMap {
+          case Valid(a)   => Future.successful(a)
+          case Invalid(e) => Future.failed(new Exception(s"$e"))
+        }
+      s <- state
+        .flatMap {
+          case Valid(a)   => Future.successful(a)
+          case Invalid(e) => Future.failed(new Exception(s"$e"))
+        }
+      savingStore <- store.insertOne(s)
+      savingJournal <- Future.successful(()) // MKDMKD todo save in journal
+    } yield Valid((e, s))
+  }
+
+  private def createACreateEvent(
+      body: Option[JsValue]
+  ): Future[ValidatedErr[ReferencesExternesFrEvent]] = {
+    body
+      .map { cmdJson =>
+        val cmd = cmdJson.as[CreateAdresseExterneFrCommand]
+        createHandler.onCommand(cmd)
+      }
+      .getOrElse(Future.successful(Invalid("bruh")))
+  }
+
+  private def fromEventToState(
+      event: Future[ValidatedErr[ReferencesExternesFrEvent]]
+  ): Future[ValidatedErr[CreateReferenceExterneFrState]] = {
+    event
+      .map {
+        case Validated.Valid(a) =>
+          a match {
+            case ref: ReferenceExterneFrCreated => Valid(reducer(ref))
+            case _                              => Invalid("Illegal transition state")
+          }
+        case i @ Invalid(e) => i
+      }
   }
 
 }
